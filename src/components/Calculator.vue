c83091365ff4787bb7ded9d264a62530fe794343
shunsa10 <sa10sh12un08@icloud.com>
2021-09-03 23:27:40 +0900

    package add

---
+++ src/components/Calculator.vue
@@ -0,0 +1,43 @@
+<template>
+<div>
+     <input class="form-control" type="text" v-model.trim="value" v-on:blur="validate" style="text-align: right">
+    <table align="center">
+    <tr>
+        <td><button @click="clear()">C</button></td>
+        <td><button @click="clearAll()">CA</button></td>
+        <td><button @click="chooseArithmetic('%');">%</button></td>
+        <td><button @click="chooseArithmetic('×');">×</button></td>
+    </tr>
+    <tr>
+        <td><button @click="chooseNumber('7');">7</button></td>
+        <td><button @click="chooseNumber('8');">8</button></td>
+        <td><button @click="chooseNumber('9');">9</button></td>
+        <td><button @click="chooseArithmetic('÷');">÷</button></td>
+    </tr>
+    <tr>
+        <td><button @click="chooseNumber('4');">4</button></td>
+        <td><button @click="chooseNumber('5');">5</button></td>
+        <td><button @click="chooseNumber('6');">6</button></td>
+        <td><button @click="chooseArithmetic('-');">-</button></td>
+    </tr>
+    <tr>
+        <td><button @click="chooseNumber('1');">1</button></td>
+        <td><button @click="chooseNumber('2');">2</button></td>
+        <td><button @click="chooseNumber('3');">3</button></td>
+        <td><button @click="chooseArithmetic('+');">+</button></td>
+    </tr>
+    <tr>
+        <td><button @click="chooseNumber('0');">0</button></td>
+        <td><button @click="chooseNumber('.');">.</button></td>
+        <td><button @click="calculationResult();">=</button></td>
+    </tr>
+</table>
+</div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue'// Vue 本体
+import App from './App.vue' //コンポーネントの読み込み
+import vuetify from './plugins/vuetify'
+import router from './router'
 
 Vue.config.productionTip = false
+// true の場合の方が開発者向けのメッセージがコンソールによりたくさん出るようです。
+
 
 new Vue({
-  render: h => h(App),
+  vuetify,
+  router,
+  render: h => h(App)
 }).$mount('#app')
+
+//render: h => h(App),は元はこれ
+//Appを生成する関数でAppを返している
+// render: function (createElement) {
+//   return createElement(App)
+// }
+
+//hというエイリアスはhyperscriptから来ており、仮想DOMの実装でよく使われるらしい。
